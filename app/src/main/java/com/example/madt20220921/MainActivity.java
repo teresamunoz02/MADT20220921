7f72e88de693580e96565aa4913b8b740ef81b4e
mtmm0021 <mtmm0021@red.ujaen.es>
2022-09-28 10:12:20 +0200

    Added change text color button

--- app/src/main/java/com/example/madt20220921/MainActivity.java
+++ app/src/main/java/com/example/madt20220921/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.madt20220921;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnChangeTextClick(View v)
     {
-        TextView tv = (TextView)findViewById(R.id.textView);
+        tv = (TextView)findViewById(R.id.textView);
         tv.setText("Welcome to android");
+        tv.setTextColor(Color.rgb(200,0,0));
+
     }
 }
